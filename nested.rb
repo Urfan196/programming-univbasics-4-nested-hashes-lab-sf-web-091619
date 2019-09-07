fab3f93af87f5968182ebe69628432af92cdf677
Urfan Guliyev <urfan196@yahoo.com>
2019-09-07 07:25:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,11 @@ def adding_matz
 # }
 
 	programmer_hash =
- 		{
+ 		{    
+			yukihiro_matsumoto: {
+				known_for: "Ruby"
+				languages: ["LISP", "C"]
+			}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -100,8 +104,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-
+		 alans_new_info = "GUI"
+programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
 
